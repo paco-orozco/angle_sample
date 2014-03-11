163233e133e22cfa755672d1ed89b41e5038af3e
fo95116 <fo95116@gmail.com>
2014-03-11 15:25:09 -0700

    modified controller and tied the functionality to the view

--- app/assets/javascripts/samples_controller.js
+++ app/assets/javascripts/samples_controller.js
@@ -1,5 +1,11 @@
 var SampleAppCtrls = angular.module("SampleAppCtrls", []);
 
 SampleAppCtrls.controller("SamplesCtrl", ["$scope", function($scope){
+  // fake samples
+  $scope.fakeSamples = [
+    {name: "bunny", description: "fluffy"},
+    {name: "Green Stuff", description: "meh"},
+    {name: "elephant", description: "big"}
+  ];
 
 }]);
