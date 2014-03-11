bfe383699583240a4ccad0aa485ebc3ac6caf107
fo95116 <fo95116@gmail.com>
2014-03-11 16:04:03 -0700

    generated a sample model

--- app/assets/javascripts/samples_controller.js
+++ app/assets/javascripts/samples_controller.js
@@ -3,10 +3,15 @@ var SampleAppCtrls = angular.module("SampleAppCtrls", []);
 SampleAppCtrls.controller("SamplesCtrl", ["$scope", "$http",
   function($scope){
   // fake samples
-    $scope.fakeSamples = [
-      {name: "bunny", description: "fluffy"},
-      {name: "Green Stuff", description: "meh"},
-      {name: "elephant", description: "big"}
-    ];
+  $scope.fakeSamples = [
+    {name: "bunny", description: "fluffy"},
+    {name: "Green Stuff", description: "meh"},
+    {name: "elephant", description: "big"}
+  ];
+  // requesting more samples
+  $http.get("/samples.json").success(function(data){
+    console.log(data);
+    $scope.fakeSamples = $scope.fakeSamples.concat(data);
+  });
 
 }]);
